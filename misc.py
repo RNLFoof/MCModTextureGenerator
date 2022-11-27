4a7973d0a75f655ea435af5bd89cf553ed443d93
RNLFoof <z.zablotsky@gmail.com>
2022-11-27 15:58:30 -0500

    Added a comment

--- misc.py
+++ misc.py
@@ -2,4 +2,6 @@ from zipfile import ZipFile
 
 
 def get_zip_assets(zip_file: ZipFile):
+    # List is used so that the totals can be displayed properly (for tqdm)
+    # Could probably be a bit slower but a bit better on memory by doing an object with len and iter but like. who cares
     return list(filter(lambda file: file.filename.startswith(r'assets/'), zip_file.filelist))
