e412e6055fbeb67068ac75a77bc654a0a0ab2054
RNLFoof <z.zablotsky@gmail.com>
2022-12-01 22:49:52 -0500

    Trying to figure out rtd

--- settings.py
+++ settings.py
@@ -1,25 +1,49 @@
 import os
+from functools import cached_property
+
+
 
 # Replace these with your own values
-from classes import Editor, Matcher
-from classes.Rule import Rule
+from typing import List
+
+from cachetools import cached
 
 mc_root = r"C:\Users\Zach\Twitch\Minecraft\Instances\5 - 1.18.2"
 resource_pack_name = r"StylizedResourcePack_x256.zip"
 vanilla_mc_version_jar = r'C:\Users\Zach\AppData\Roaming\.minecraft\versions\1.18.2\1.18.2.jar'
 
-rules = [
-    Rule(
-        Matcher.RegexMatcher(r"assets.quark.textures.block.+?corundum_cluster\.png"),
-        Matcher.EndingMatcher("amethyst_cluster.png"),
-        Editor.TransferPaletteEditor
-    ),
-    Rule(
-        Matcher.RegexMatcher(r"assets.quark.textures.block.+?corundum\.png"),
-        Matcher.EndingMatcher("amethyst_block.png"),
-        Editor.TransferPaletteEditor
-    ),
-]
+
+def rules():
+    print("hey")
+    from classes.Source import Source
+    from classes import Editor, Matcher
+    from classes.Rule import Rule
+
+    return [
+        Rule(
+            (
+                Matcher.RegexMatcher(Source.MOD, r"assets.quark.textures.block.+?corundum_cluster\.png"),
+                Matcher.EndingMatcher(Source.PACK, "amethyst_cluster.png"),
+            ),
+            Editor.TransferPaletteEditor
+        ),
+        Rule(
+            (
+                Matcher.RegexMatcher(Source.MOD, r"assets.quark.textures.block.+?corundum\.png"),
+                Matcher.EndingMatcher(Source.PACK, "amethyst_block.png"),
+            ),
+            Editor.TransferPaletteEditor
+        ),
+        # When mod matches vanilla, transfer from mod to pack
+        Rule(
+            (
+                Matcher.RegexMatcher(Source.MOD, r"assets.quark.textures.block.+?corundum\.png"),
+                Matcher.EndingMatcher(Source.PACK, "amethyst_block.png"),
+            ),
+            Editor.TransferPaletteEditor
+        ),
+    ]
+
 
 # Leave these alone
 mc_mods = os.path.join(mc_root, "mods")
